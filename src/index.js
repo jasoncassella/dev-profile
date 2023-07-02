b3a80c35fa18254161b36c24942df731323a07a8
Jason Cassella <jasoncassella1@gmail.com>
2023-07-02 12:39:32 -0400

    react components make sense im ngl

--- src/index.js
+++ src/index.js
@@ -1,17 +1,58 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './styles.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function App() {
+	return (
+		<div className='card'>
+			<Avatar />
+			<div className='data'>
+				<Intro />
+				<SkillList />
+			</div>
+		</div>
+	);
+}
+
+function Avatar() {
+	return <img className='avatar' src='kirby.jpg' alt='kirby' />;
+}
+
+function Intro() {
+	return (
+		<div>
+			<h1>Jason Cassella</h1>
+			<p>Help desk guy tryna get good at react</p>
+		</div>
+	);
+}
+
+function SkillList() {
+	return (
+		<div className='skill-list'>
+			<Skill name='HTML + CSS' style={{ background: 'lightgreen' }} />
+			<Skill name='JavaScript' style={{ background: 'yellow' }} />
+			<Skill name='React' style={{ background: 'lightblue' }} />
+			<Skill name='Git' style={{ background: 'orange' }} />
+			<Skill name='Python' style={{ background: 'purple' }} />
+			<Skill name='Web Design' style={{ background: 'red' }} />
+		</div>
+	);
+}
+
+function Skill(props) {
+	return (
+		<div className='skill' style={props.style}>
+			{props.name}
+		</div>
+	);
+}
+
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<StrictMode>
+		<App />
+	</StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
